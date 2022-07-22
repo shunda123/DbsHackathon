6390cdcef665e164ea32706dbcabb3cff8dae54e
shunda <yarshunda995@gmail.com>
2022-07-22 08:40:08 +0800

    initial commit

---
+++ src/main/java/com/dbs/hackathon/seed/SeedApplication.java
@@ -0,0 +1,13 @@
+package com.dbs.hackathon.seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbs/hackathon/seed/SeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbs.hackathon.seed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
